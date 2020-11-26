a9e3f3fea245535ddec863a25a6a5f20bad0214f
Jonathan Cain <jonathan_m_cain@hotmail.co.uk>
2020-11-26 21:47:07 +0000

    Made some changes

--- src/player.js
+++ src/player.js
@@ -4,6 +4,8 @@ const Character = require('./character');
 function Player(config) {
   Character.call(this, config);
   this.equippedWeapon = null;
+  this.nextLevel = 1000;
+  this.currentExperience = 0;
 }
 
 Player.prototype = Object.create(Character.prototype);
@@ -23,8 +25,21 @@ Player.prototype.equip = function (weapon) {
 };
 
 Player.prototype.attack = function (target) {
-  target._takeDamage(this.equippedWeapon.damage);
+  target._takeDamage(this.attackTotal + this.equippedWeapon.damage);
+  this.currentExperience += target.experienceReward;
   return this._describeAttack(target);
+
+  // After a killing blow and experience reward, Players should check if their currentExperience is equal to or greater than their nextLevel property. If so, the levelUp() method should be called, and a new nextLevel target that is 10% larger should be set.
+  if (this.currentExperience >= this.nextLevel) {
+    this.levelUp();
+    this.nextLevel *= 1.1;
+  }
+
+};
+
+Player.prototype.levelUp = function () {
+  this.level += 1;
+
 };
 
 module.exports = Player;
